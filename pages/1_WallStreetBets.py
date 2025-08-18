c0c3cbe6a98c786d95bb977da75d2b9a9d5dd95d
etensoep2 <etensoep@gmail.com>
2025-08-18 21:50:00 +0200

    Update 1_WallStreetBets.py

--- pages/1_WallStreetBets.py
+++ pages/1_WallStreetBets.py
@@ -7,9 +7,9 @@ st.title("Wall Street Bets")
 
 stock1 = yf.Ticker("PLTR")
 info1 = stock1.get_info()
-company_name1 = info1.get("longName", "N/A")
-current_price1 = info1.get("currentPrice", "N/A")
-pe_ratio1 = info1.get("trailingPE", "N/A")
+company_name1 = info1.get("longName", stock1)
+current_price1 = info1.get("currentPrice", stock1)
+pe_ratio1 = info1.get("trailingPE", stock1)
 
 st.write(f"### {company_name1} ({ticker})")
 st.write(f"💰 Current Price: ${current_price1}")
